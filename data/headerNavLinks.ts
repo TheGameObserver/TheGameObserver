9a94b0835a048fb3a46722bc438b5c8c554c7671
the-game-observer <styris.paul0729@gmail.com>
2026-07-01 21:06:02 +0530

    Update branding

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  { href: '/blog', title: 'Analysis' },
+  { href: '/tags', title: 'Competitions' },
   { href: '/about', title: 'About' },
 ]
 
