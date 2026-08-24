89152a9b813467ca240292f1c6be4c94f9521c91
the-game-observer <styris.paul0729@gmail.com>
2026-08-24 12:31:59 +0000

    Replace header logo with Game Observer branding

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+import Image from './Image'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
@@ -17,7 +17,12 @@ const Header = () => {
       <Link href="/" aria-label={siteMetadata.headerTitle}>
         <div className="flex items-center justify-between">
           <div className="mr-3">
-            <Logo />
+            <Image
+              src="/images/the-game-observer-logo.png"
+              alt="The Game Observer logo"
+              width={44}
+              height={44}
+            />
           </div>
           {typeof siteMetadata.headerTitle === 'string' ? (
             <div className="hidden h-6 text-2xl font-semibold sm:block">
