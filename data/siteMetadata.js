cef842a1ba88c3d47f5864e4db7811ed993b8596
the-game-observer <styris.paul0729@gmail.com>
2026-08-27 18:35:55 +0000

    Clean starter branding and demo content

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,27 +1,16 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
   title: 'The Game Observer',
-  author: 'Sty Paul',
+  author: 'The Game Observer',
   headerTitle: 'The Game Observer',
-  description: 'Football Analysis • Tactical Insights • Coaching Education',
+  description: 'A professional global football analysis publication focused on tactical, technical and analytical insight.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteRepo: 'https://github.com/TheGameObserver/TheGameObserver',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
-  mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  x: 'https://twitter.com/x',
-  // twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
-  threads: 'https://www.threads.net',
-  instagram: 'https://www.instagram.com',
-  medium: 'https://medium.com',
-  bluesky: 'https://bsky.app/',
+  // Social profiles intentionally omitted until official accounts exist.
   locale: 'en-US',
   // set to true if you want a navbar fixed to the top
   stickyNav: false,
