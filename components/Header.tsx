2b7acc027a0e8633e33f961b5ff0c9497bbc2d17
the-game-observer <styris.paul0729@gmail.com>
2026-09-23 00:08:54 +0530

    fix: install approved TGO logo asset (#2)

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,7 +17,7 @@ const Header = () => {
       <Link href="/" aria-label={siteMetadata.headerTitle}>
         <div className="flex items-center">
           <Image
-            src="/images/the-game-observer-logo.png"
+            src="/images/the-game-observer-logo.jpg"
             alt="The Game Observer"
             width={180}
             height={120}
